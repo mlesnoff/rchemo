c1692a164ce64bf280248e8c5e8146ca2b5c3314
lesnoff <matthieu.lesnoff@cirad.fr>
2021-04-26 15:24:21 +0200

    0.0-0

---
+++ R/checking.R
@@ -0,0 +1,45 @@
+checkna <- function(X) {
+    X <- .mat(X)
+    n <- dim(X)[1]
+    z <- rowSums(is.na(X))
+    z <- data.frame(rownum = seq_len(n), rownam = row.names(X), nbna = z)
+    z$all.na <- ifelse(z$nbna == ncol(X), TRUE, FALSE)
+    z
+    }
+  
+checkdupl <- function(X, Y = NULL, digits = NULL) {
+    X <- as.data.frame(.mat(X))
+    if(is.null(Y))
+        zY <- X
+    else
+        zY <- as.data.frame(.mat(Y))
+    n <- dim(X)[1]
+    m <- dim(zY)[1]
+    nam <- colnames(X)
+    X$rownum1 <- seq_len(n)
+    zY$rownum2 <- seq_len(m)
+    if(!is.null(digits)) {
+        X <- round(X, digits = digits)
+        zY <- round(zY, digits = digits)
+        }
+    z <- merge(X, zY, by = nam, all = FALSE, allow.cartesian = TRUE)
+    nam <- c("rownum1", "rownum2", nam)
+    z <- z[, nam]
+    if(is.null(Y)) {
+        z <- z[z$rownum1 != z$rownum2, ]
+        v <- z[, c("rownum1", "rownum2")]
+        for(i in seq_len(dim(v)[1])) 
+          v[i, ] <- sort(v[i, ])
+        z <- merge(unique(v), z, all = FALSE)
+        }
+    z <- z[order(z$rownum1), ]
+    row.names(z) <- seq_len(dim(z)[1])
+    if(dim(z)[1] == 0)
+        z <- NULL
+    z
+    }
+
+
+
+
+
